9de0e1fd8e11004a35bb93dc7eeb43276663bf8c
Drew Price <drewprice@users.noreply.github.com>
2015-09-13 12:26:11 -0400

    Tubezed

--- public/fetch_video.js
+++ public/fetch_video.js
@@ -1,25 +1,24 @@
-// document ready means that this script will wait 
-// for the page to get loaded before running
 $(document).ready(function() {
 
-  // user clicks button with cursor
   $("#user-submit").click(function() {
     ajaxCall();
   });
 
-  // user hits enter while on the input field
   $('#text-field').keypress(function(e){
     if(e.which == 13){
       e.preventDefault();
       e.stopPropagation();
+
       ajaxCall();
     }
   });
-
-  function ajaxCall() {
-    
-    // your code will go here
-
-  }
-
 });
+
+function ajaxCall() {
+  var searchTerm = $('#text-field').val();
+
+  $.get('/widget', { search_keyword: searchTerm }, function (data) {
+    $('#ytplayer').remove();
+    $('#search-results').append(data);
+  })
+}
